1f300238640482b3edab8f84cc3514677fb14dfc
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-17 23:37:31 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,12 @@
 class Transfer
   
-  def initialize(receiver, giver, amount)
-    
+  attr_accessor :amount, :status
+  
+  def initialize(receiver, sender, amount)
+    @receiver = receiver
+    @sender = sender
+    @amount = 50 
+    @status = "pending"
   end 
   
 end
